966dd6f87be2b2208a3d3de5eed4dc56cf3560ed
GlebasusBP <bgp13art@Gmail.com>
2023-01-23 14:10:06 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.candidate = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
 module.exports = GuessingGame;
+
